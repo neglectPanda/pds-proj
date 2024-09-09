06a70a61e6be1978f62666a65378b7310201dc31
neglectPanda <mypacc006@gmail.com>
2024-09-10 07:02:48 +0800

    added images and font, made new page.tsx, components folder and pages folder. pages folder has 3 other page folders

---
+++ app/images/1image.js
@@ -0,0 +1,44 @@
+import mainLogo from './1.png';
+import submittedFormLogo from './2.png';
+import personalSheetLogo from './Your Data logo.png';
+import aboutLogo from './about logo.png';
+import activitiesLogo from './activities logo.png';
+import userListLogo from './user list.png';
+import requestsLogo from './requests.png';
+import analyticsLogo from './analytics.png';
+import settingsLogo from './settings.png';
+import mainMenuLogo from './main menu logo.png';
+import logoutLogo from './logout.png';
+import totalUserCountLogo from './total-user-count.png';
+import activeUserCountLogo from './active user count.png';
+import inactiveUserCountLogo from './inactive user count.png';
+import adminUserCountLogo from './admin user count.png';
+import baseUserCountLogo from './normal user count.png';
+import accountSettingsLogo from './account settings logo.png';
+import notificationLogo from './notification logo.png';
+import blockedUserListLogo from './blocked user list.png';
+import helpAndSupportLogo from './help and support.png';
+
+
+export {
+    mainLogo,
+    submittedFormLogo,
+    personalSheetLogo,
+    aboutLogo,
+    activitiesLogo, 
+    userListLogo,
+    requestsLogo,
+    analyticsLogo,
+    settingsLogo,
+    mainMenuLogo,
+    logoutLogo,
+    totalUserCountLogo,
+    activeUserCountLogo,
+    inactiveUserCountLogo,
+    adminUserCountLogo,
+    baseUserCountLogo,
+    accountSettingsLogo,
+    notificationLogo,
+    blockedUserListLogo,
+    helpAndSupportLogo
+};
