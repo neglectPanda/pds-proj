b0118042c55776203e119e160ebd6c7a64d949f7
neglectPanda <mypacc006@gmail.com>
2024-09-11 10:14:57 +0800

    Transferred images and fonts folder to public folder --this was made--; icon for website now visible

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <head>
                 <title>Personal Data Sheet</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="./images/1.png" />
+                <link rel="icon" href="/images/1.png" />
             </head>
             <body>
                 {children}
